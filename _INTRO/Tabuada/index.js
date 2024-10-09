a8f56b6196b7efc9fe1a1554fb50b297b1a17243
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-09 18:22:15 -0300

    Tabuada

--- _INTRO/Tabuada/index.js
+++ _INTRO/Tabuada/index.js
@@ -2,8 +2,8 @@ const tabuadaDo = 5;
 
 function tabuada(n) {
 
-    for(let i = 0; i <= 10; i++){
-        console.log(i + ' x ' + n + ': ' + (i * n))
+    for(let i = 0; i <= 10; i++) {
+        console.log(i + ' x ' + n + ': ' + (i * n));
     }
 }
 tabuada(tabuadaDo);
