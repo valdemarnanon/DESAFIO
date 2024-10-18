b113ecb74a514bcef4e4cf72253625b68cd3c4ac
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-10-18 15:38:40 -0300

    Filtrando

--- _ArrayObjetos/Filtrando propriedades/index.js
+++ _ArrayObjetos/Filtrando propriedades/index.js
@@ -1,7 +1,22 @@
-const obj = {
+const pessoa = {
     nome: 'Valdemar',
     sobrenome: 'Nanon',
     idade: 21,
     sexo: 'gay',
+};
+
+function filtrarPropriedades(obj, propriedadesPermitidas) {
+    const novoObj = {}
+
+    for(const prop of propriedadesPermitidas) {
+        if(obj.hasOwnProperty(prop)) {
+            novoObj[prop] = obj[prop];
+        }
+    }
+    return novoObj;
 }
 
+const propriedadesPermitidas = ['nome', 'sobrenome', 'sexo'];
+
+const pessoaFiltrada = filtrarPropriedades(pessoa, propriedadesPermitidas);
+console.log(pessoaFiltrada)
